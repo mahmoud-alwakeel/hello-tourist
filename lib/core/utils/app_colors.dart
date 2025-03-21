d27bdb9988ed42765f74c66504209d0472d3513a
mahmoud-alwakeel <mahmoudalwakeel200@gmail.com>
2025-03-21 14:41:31 +0200

    main files

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static Color primaryColor = const Color(0xffC49E85);
+  static Color backgroundColor = const Color(0xffF8F4F9);
+  static Color deepBrown = const Color(0xff6B4B3E);
+}
---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  static const String assetsImagesOnboardingOne = "assets/images/onboarding_one.png";
+
+  static const String assetsImagesOnboardingThree = "assets/images/onboarding_three.png";
+
+  static const String assetsImagesOnboardingTwo = "assets/images/onboarding_two.png";
+}
+
