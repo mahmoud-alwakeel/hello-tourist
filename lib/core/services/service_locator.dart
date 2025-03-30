31284c2f461924c5a1b7a06555d9079a00443fc6
mahmoud-alwakeel <mahmoudalwakeel200@gmail.com>
2025-03-30 23:06:33 +0200

    add firebase to project

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,7 +1,9 @@
 import 'package:get_it/get_it.dart';
 import 'package:hello_tourist/core/database/cache/cahce_helper.dart';
+import 'package:hello_tourist/feautres/auth/presentation/auth_cubit/auth_cubit.dart';
 
 final getIt = GetIt.instance;
 void setupServiceLocator() {
   getIt.registerSingleton<CacheHelper>(CacheHelper());
+  getIt.registerSingleton<AuthCubit>(AuthCubit());
 }
